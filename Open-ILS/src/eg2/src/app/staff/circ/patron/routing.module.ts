5c06eb55c0b50da5f3d29b0bb9dfc33663109179
Bill Erickson <berickxx@gmail.com>
2020-08-12 12:50:59 -0400

    LP1904036 Angular Patron UI initial structures
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/src/eg2/src/app/staff/circ/patron/routing.module.ts
+++ Open-ILS/src/eg2/src/app/staff/circ/patron/routing.module.ts
@@ -1,16 +1,25 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {PatronComponent} from './patron.component';
 
-const routes: Routes = [
-  { path: 'bcsearch',
-    loadChildren: () =>
-      import('./bcsearch/bcsearch.module').then(m => m.BcSearchModule)
-  }
-];
+const routes: Routes = [{
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'search'
+  }, {
+    path: 'search',
+    component: PatronComponent
+  }, {
+    path: 'bcsearch',
+    component: PatronComponent
+  }, {
+    path: ':id/:tab',
+    component: PatronComponent,
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 
-export class CircPatronRoutingModule {}
+export class PatronRoutingModule {}
