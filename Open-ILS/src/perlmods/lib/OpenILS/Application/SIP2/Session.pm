befaa5511f13fbb3650aac92f7ebda49f53104f9
Bill Erickson <berickxx@gmail.com>
2020-09-04 10:09:15 -0400

    migrating top sip2 service
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/src/perlmods/lib/OpenILS/Application/SIP2/Session.pm
+++ Open-ILS/src/perlmods/lib/OpenILS/Application/SIP2/Session.pm
@@ -1,21 +1,3 @@
-# ---------------------------------------------------------------
-# Copyright (C) 2020 King County Library System
-# Bill Erickson <berickxx@gmail.com>
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# ---------------------------------------------------------------
-# Code borrows heavily and sometimes copies directly from from
-# ../SIP* and SIPServer*
-# ---------------------------------------------------------------
 package OpenILS::Application::SIPSession;
 use strict; use warnings;
 use JSON::XS;
