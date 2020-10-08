a69005cd345f85f46344f56153debde1a09ab2e7
Bill Erickson <berickxx@gmail.com>
2020-10-08 18:22:12 -0400

    LPXXX ACQ Selection List & PO Angluar Port
    New selection list UI
    New PO UI
    New Lineitem worksheet UI with stub print template
    New PO print UI with stub print template
    New brief record UI
    
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/src/eg2/src/app/share/item-location-select/item-location-select.component.ts
+++ Open-ILS/src/eg2/src/app/share/item-location-select/item-location-select.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit, AfterViewInit, Input, Output, ViewChild,
     EventEmitter, forwardRef} from '@angular/core';
 import {ControlValueAccessor, FormGroup, FormControl, NG_VALUE_ACCESSOR} from '@angular/forms';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {Observable, from, of} from 'rxjs';
+import {tap, map, switchMap} from 'rxjs/operators';
 import {IdlObject} from '@eg/core/idl.service';
 import {OrgService} from '@eg/core/org.service';
 import {AuthService} from '@eg/core/auth.service';
@@ -10,6 +10,7 @@ import {PermService} from '@eg/core/perm.service';
 import {PcrudService} from '@eg/core/pcrud.service';
 import {ComboboxComponent, ComboboxEntry} from '@eg/share/combobox/combobox.component';
 import {StringComponent} from '@eg/share/string/string.component';
+import {ItemLocationService} from './item-location-select.service';
 
 /**
  * Item (Copy) Location Selector.
@@ -30,6 +31,7 @@ import {StringComponent} from '@eg/share/string/string.component';
 })
 export class ItemLocationSelectComponent
     implements OnInit, AfterViewInit, ControlValueAccessor {
+    static domIdAuto = 0;
 
     // Limit copy locations to those owned at or above org units where
     // the user has work permissions for the provided permission code.
@@ -45,30 +47,49 @@ export class ItemLocationSelectComponent
 
     @Input() required: boolean;
 
+    @Input() domId = 'eg-item-location-select-' +
+        ItemLocationSelectComponent.domIdAuto++;
+
+    // If false, selector will be click-able
+    @Input() loadAsync = true;
+
+    @Input() disabled = false;
+
+    // Display the selected value as text instead of within
+    // the typeahead
+    @Input() readOnly = false;
+
     @ViewChild('comboBox', {static: false}) comboBox: ComboboxComponent;
     @ViewChild('unsetString', {static: false}) unsetString: StringComponent;
 
-    startId: number = null;
+    @Input() startId: number = null;
     filterOrgs: number[] = [];
-    cache: {[id: number]: IdlObject} = {};
+    filterOrgsApplied = false;
 
     initDone = false; // true after first data load
     propagateChange = (id: number) => {};
     propagateTouch = () => {};
 
+    getLocationsAsyncHandler = term => this.getLocationsAsync(term);
+
     constructor(
         private org: OrgService,
         private auth: AuthService,
         private perm: PermService,
-        private pcrud: PcrudService
+        private pcrud: PcrudService,
+        private loc: ItemLocationService
     ) {
         this.valueChange = new EventEmitter<IdlObject>();
     }
 
     ngOnInit() {
-        this.setFilterOrgs()
-        .then(_ => this.getLocations())
-        .then(_ => this.initDone = true);
+        if (this.loadAsync) {
+            this.initDone = true;
+        } else {
+            this.setFilterOrgs()
+            .then(_ => this.getLocations())
+            .then(_ => this.initDone = true);
+        }
     }
 
     ngAfterViewInit() {
@@ -114,12 +135,64 @@ export class ItemLocationSelectComponent
 
         return this.pcrud.search('acpl', search, {order_by: {acpl: 'name'}}
         ).pipe(map(loc => {
-            this.cache[loc.id()] = loc;
+            this.loc.locationCache[loc.id()] = loc;
             entries.push({id: loc.id(), label: loc.name(), userdata: loc});
         })).toPromise().then(_ => {
             this.comboBox.entries = entries;
         });
     }
+
+    getLocationsAsync(term: string): Observable<ComboboxEntry> {
+
+        let obs = of();
+        if (!this.filterOrgsApplied) {
+            // Apply filter orgs the first time they are needed.
+            obs = from(this.setFilterOrgs());
+        }
+
+        return obs.pipe(switchMap(_ => this.getLocationsAsync2(term)));
+    }
+
+    getLocationsAsync2(term: string): Observable<ComboboxEntry> {
+
+        if (this.filterOrgs.length === 0) {
+            return of();
+        }
+
+        const search: any = {
+            deleted: 'f',
+            name: {'ilike': `%${term}%`}
+        };
+
+        if (this.startId) {
+            // Guarantee we have the load-time copy location, which
+            // may not be included in the org-scoped set of locations
+            // we fetch by default.
+            search['-or'] = [
+                {id: this.startId},
+                {owning_lib: this.filterOrgs}
+            ];
+        } else {
+            search.owning_lib = this.filterOrgs;
+        }
+
+        return new Observable<ComboboxEntry>(observer => {
+            if (!this.required) {
+                observer.next({id: null, label: this.unsetString.text});
+            }
+
+            this.pcrud.search('acpl', search, {order_by: {acpl: 'name'}}
+            ).subscribe(
+                loc => {
+                    this.loc.locationCache[loc.id()] = loc;
+                    observer.next({id: loc.id(), label: loc.name(), userdata: loc});
+                },
+                err => {},
+                () => observer.complete()
+            );
+        });
+    }
+
 
     registerOnChange(fn) {
         this.propagateChange = fn;
@@ -132,7 +205,7 @@ export class ItemLocationSelectComponent
     cboxChanged(entry: ComboboxEntry) {
         const id = entry ? entry.id : null;
         this.propagateChange(id);
-        this.valueChange.emit(id ? this.cache[id] : null);
+        this.valueChange.emit(id ? this.loc.locationCache[id] : null);
     }
 
     writeValue(id: number) {
@@ -144,26 +217,56 @@ export class ItemLocationSelectComponent
     }
 
     getOneLocation(id: number) {
-        if (!id || this.cache[id]) { return Promise.resolve(); }
+        if (!id) { return Promise.resolve(); }
 
-        return this.pcrud.retrieve('acpl', id).toPromise()
-        .then(loc => {
-            this.cache[loc.id()] = loc;
-            this.comboBox.entries.push(
-                {id: loc.id(), label: loc.name(), userdata: loc});
+        const promise = this.loc.locationCache[id] ?
+            Promise.resolve(this.loc.locationCache[id]) :
+            this.pcrud.retrieve('acpl', id).toPromise();
+
+        return promise.then(loc => {
+
+            this.loc.locationCache[loc.id()] = loc;
+            const entry: ComboboxEntry = {
+                id: loc.id(), label: loc.name(), userdata: loc};
+
+            if (this.comboBox.entries) {
+                this.comboBox.entries.push(entry);
+            } else {
+                this.comboBox.entries = [entry];
+            }
         });
     }
 
     setFilterOrgs(): Promise<number[]> {
-        if (this.permFilter) {
-            return this.perm.hasWorkPermAt([this.permFilter], true)
-                .then(values => this.filterOrgs = values[this.permFilter]);
+        const org = this.contextOrgId || this.auth.user().ws_ou();
+        const contextOrgIds = this.org.ancestors(org, true);
+        this.filterOrgsApplied = true;
+
+        if (!this.permFilter) {
+            return Promise.resolve(this.filterOrgs = contextOrgIds);
         }
 
-        const org = this.contextOrgId || this.auth.user().ws_ou();
-        this.filterOrgs = this.org.ancestors(this.contextOrgId, true);
+        const orgsFromCache = this.loc.filterOrgsCache[this.permFilter];
+        if (orgsFromCache) {
+            return Promise.resolve(this.filterOrgs = orgsFromCache);
+        }
 
-        return Promise.resolve(this.filterOrgs);
+        return this.perm.hasWorkPermAt([this.permFilter], true)
+        .then(values => {
+            // Always limit the org units to /at most/ those within
+            // scope of the context org ID.
+
+            const permOrgIds = values[this.permFilter];
+            const trimmedOrgIds = [];
+            permOrgIds.forEach(orgId => {
+                if (contextOrgIds.includes(orgId)) {
+                    trimmedOrgIds.push(orgId);
+                }
+            });
+
+            this.loc.filterOrgsCache[this.permFilter] = trimmedOrgIds;
+            return this.filterOrgs = trimmedOrgIds;
+        });
     }
 
     orgName(orgId: number): string {
