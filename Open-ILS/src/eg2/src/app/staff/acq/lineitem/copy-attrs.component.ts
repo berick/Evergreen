a04fb0a7fa1412daaaef5a2ab35345311efd56c6
Bill Erickson <berickxx@gmail.com>
2020-12-21 15:49:34 -0500

    LPXXX Batch copy updates WIP
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/src/eg2/src/app/staff/acq/lineitem/copy-attrs.component.ts
+++ Open-ILS/src/eg2/src/app/staff/acq/lineitem/copy-attrs.component.ts
@@ -6,6 +6,7 @@ import {NetService} from '@eg/core/net.service';
 import {AuthService} from '@eg/core/auth.service';
 import {LineitemService} from './lineitem.service';
 import {ComboboxEntry} from '@eg/share/combobox/combobox.component';
+import {ItemLocationService} from '@eg/share/item-location-select/item-location-select.service';
 
 @Component({
   templateUrl: 'copy-attrs.component.html',
@@ -13,16 +14,39 @@ import {ComboboxEntry} from '@eg/share/combobox/combobox.component';
 })
 export class LineitemCopyAttrsComponent implements OnInit {
 
-    @Input() copy: IdlObject; // acqlid
+    fundEntries: ComboboxEntry[];
+    circModEntries: ComboboxEntry[];
+
+    private _copy: IdlObject;
+    @Input() set copy(c: IdlObject) { // acqlid
+        if (c === undefined) {
+            return;
+        } else if (c === null) {
+            this._copy = null;
+        } else {
+            // Enture cbox entries are populated before the copy is
+            // applied so the cbox has the minimal set of values it
+            // needs at copy render time.
+            this.setInitialOptions(c);
+            this._copy = c;
+        }
+    }
+
+    get copy(): IdlObject {
+        return this._copy;
+    }
+
     @Input() batchMode = false;
 
     // Emits an 'acqlid' object;
     @Output() saveRequested: EventEmitter<IdlObject> = new EventEmitter<IdlObject>();
 
+
     constructor(
         private idl: IdlService,
         private net: NetService,
         private auth: AuthService,
+        private loc: ItemLocationService,
         private liService: LineitemService
     ) {}
 
@@ -31,6 +55,34 @@ export class LineitemCopyAttrsComponent implements OnInit {
             // Will be no copy in the batch edit row
             this.copy = this.idl.create('acqlid');
             this.copy.isnew(true);
+        }
+    }
+
+    // Tell our inputs about the values we know we need,
+    // then de-flesh the items for consistency
+    setInitialOptions(copy: IdlObject) {
+        if (copy.fund()) {
+            this.fundEntries = [{
+                id: copy.fund().id(),
+                label: copy.fund().name(),
+                fm: copy.fund()
+            }];
+            copy.fund(copy.fund().id());
+        }
+
+        if (copy.circ_modifier()) {
+            this.circModEntries = [{
+                id: copy.circ_modifier().code(),
+                label: copy.circ_modifier().name(),
+                fm: copy.circ_modifier()
+            }];
+            copy.circ_modifier(copy.circ_modifier().code());
+        }
+
+        if (copy.location()) {
+            // This comp is a cbox wrapper and has its own cache
+            this.loc.locationCache[copy.location().id()] = copy.location();
+            copy.location(copy.location().id());
         }
     }
 }
