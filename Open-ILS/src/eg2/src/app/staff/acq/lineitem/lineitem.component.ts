b0a87b46fd637e27b9dc933c70318a083c6dbf82
Bill Erickson <berickxx@gmail.com>
2020-12-23 10:33:37 -0500

    LPXXX Lineitem list WIP
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/src/eg2/src/app/staff/acq/lineitem/lineitem.component.ts
+++ Open-ILS/src/eg2/src/app/staff/acq/lineitem/lineitem.component.ts
@@ -4,9 +4,6 @@ import {Component, OnInit} from '@angular/core';
   templateUrl: 'lineitem.component.html'
 })
 export class LineitemComponent implements OnInit {
-    // TODO: maybe add a summary display?
-
-    ngOnInit() {
-    }
+    ngOnInit() {}
 }
 
