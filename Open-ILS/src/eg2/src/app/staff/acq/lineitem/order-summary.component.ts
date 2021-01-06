e3c1c8032fdd70fd71ac66d6c23c355615f31e72
Bill Erickson <berickxx@gmail.com>
2021-01-06 15:18:32 -0500

    LPXXX LI 'paid' indicator
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/src/eg2/src/app/staff/acq/lineitem/order-summary.component.ts
+++ Open-ILS/src/eg2/src/app/staff/acq/lineitem/order-summary.component.ts
@@ -7,5 +7,16 @@ import {IdlObject} from '@eg/core/idl.service';
 })
 export class LineitemOrderSummaryComponent {
     @Input() li: IdlObject;
+
+    // True if at least one item has been invoiced and all items are either
+    // invoiced or canceled.
+    paidOff(): boolean {
+        const sum = this.li.order_summary();
+        return (
+            sum.invoice_count() > 0 && (
+                sum.item_count() === (sum.invoice_count() + sum.cancel_count())
+            )
+        );
+    }
 }
 
