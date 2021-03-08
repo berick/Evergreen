b106d0b54c9d0fde9aa170d1ba76a72b3bfc1832
Bill Erickson <berickxx@gmail.com>
2021-03-08 11:57:19 -0500

    LP1904036 Billing continued
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/src/perlmods/lib/OpenILS/Application/Actor.pm
+++ Open-ILS/src/perlmods/lib/OpenILS/Application/Actor.pm
@@ -5289,7 +5289,6 @@ sub user_billing_xacts {
                         target_copy 
                         workstation 
                         checkin_workstation 
-                        circ_lib
                     /],
 				    acp =>  [qw/
 					    call_number
