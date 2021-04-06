e961646726df024930ebbdff6beb4d667e39b6f9
Bill Erickson <berickxx@gmail.com>
2021-04-06 11:26:48 -0400

    LP1904036 structural stuff; message actions
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/src/eg2/src/app/staff/share/patron/penalty-dialog.component.ts
+++ Open-ILS/src/eg2/src/app/staff/share/patron/penalty-dialog.component.ts
@@ -35,14 +35,14 @@ export class PatronPenaltyDialogComponent
     SILENT_NOTE = 21;
     STAFF_CHR = 25;
 
-    staffInitials: string;
+    penalty: IdlObject; // modifying an existing penalty
     penaltyTypes: IdlObject[];
     penaltyTypeFromSelect = '';
-    penaltyTypeFromButton;
+    penaltyTypeFromButton: number;
     patron: IdlObject;
     dataLoaded = false;
     requireInitials = false;
-    initials: string;
+    initials: string; // TODO
     noteText = '';
 
     @ViewChild('successMsg', {static: false}) successMsg: StringComponent;
@@ -68,7 +68,21 @@ export class PatronPenaltyDialogComponent
     init(): Observable<any> {
         this.dataLoaded = false;
 
-        this.penaltyTypeFromButton = this.SILENT_NOTE;
+        if (this.penalty) { // Modifying an existing penalty
+            const pen = this.penalty;
+            const sp = pen.standing_penalty().id();
+            if (sp === this.ALERT_NOTE ||
+                sp === this.SILENT_NOTE || sp === this.STAFF_CHR) {
+                this.penaltyTypeFromButton = sp;
+            } else {
+                this.penaltyTypeFromSelect = sp;
+            }
+
+            this.noteText = pen.note();
+
+        } else {
+            this.penaltyTypeFromButton = this.SILENT_NOTE;
+        }
 
         this.org.settings(['ui.staff.require_initials.patron_standing_penalty'])
         .then(sets => this.requireInitials =
@@ -89,7 +103,32 @@ export class PatronPenaltyDialogComponent
         }));
     }
 
+    modifyPenalty() {
+        this.penalty.note(this.initials ?
+            `${this.noteText} [${this.initials}]` : this.noteText);
+
+        this.penalty.standing_penalty(
+            this.penaltyTypeFromSelect || this.penaltyTypeFromButton);
+
+        this.pcrud.update(this.penalty).toPromise()
+        .then(ok => {
+            if (!ok) {
+                this.errorMsg.current().then(msg => this.toast.danger(msg));
+                this.error('Update failed', true);
+            } else {
+                this.successMsg.current().then(msg => this.toast.success(msg));
+                this.penalty = null;
+                this.close(ok);
+            }
+        });
+    }
+
     apply() {
+
+        if (this.penalty) {
+            this.modifyPenalty();
+            return;
+        }
 
         const pen = this.idl.create('ausp');
         pen.usr(this.patronId);
