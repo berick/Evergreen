5d677c0a7fe81aef6cf15aab7f95712dc6c43a49
Bill Erickson <berickxx@gmail.com>
2021-04-09 15:38:57 -0400

    LP1904036 checkin UI
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/src/eg2/src/app/staff/circ/routing.module.ts
+++ Open-ILS/src/eg2/src/app/staff/circ/routing.module.ts
@@ -1,12 +1,15 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [
-  { path: 'patron',
-    loadChildren: () =>
-      import('./patron/patron.module').then(m => m.PatronManagerModule)
-  }
-];
+const routes: Routes = [{
+  path: 'patron',
+  loadChildren: () =>
+    import('./patron/patron.module').then(m => m.PatronManagerModule)
+}, {
+  path: 'checkin',
+  loadChildren: () =>
+    import('./checkin/checkin.module').then(m => m.CheckinModule)
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
